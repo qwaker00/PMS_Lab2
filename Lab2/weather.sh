a74bca7b71161163b564b9c3056a5b5ccefed7cf
Eugene Gritskevich <qwaker.00@gmail.com>
2013-03-19 18:21:37 +0300

    Remove temp file

--- Lab2/weather.sh
+++ Lab2/weather.sh
@@ -5,10 +5,9 @@ echo 'Обновление :' $timeout 'секунд'
 
 while [[ 1 ]]
 do
-    wget -q -O .weather.tmp http://export.yandex.ru/weather-ng/forecasts/26850.xml
-    weather=`head -n30 .weather.tmp | grep '\<weather_type\>' | awk -F"[><]" '{print $3}'`
-    temperature=`head -n30 .weather.tmp | grep temperature | awk -F"[><]" '{print $3}'`
-    rm .weather.tmp
+    data=`wget -q -O .weather.tmp http://export.yandex.ru/weather-ng/forecasts/26850.xml`
+    weather=`echo "$data" | head -n30 .weather.tmp | grep '\<weather_type\>' | awk -F"[><]" '{print $3}'`
+    temperature=`echo "$data" | head -n30 .weather.tmp | grep temperature | awk -F"[><]" '{print $3}'`
 
     echo 'Минск:' $weather', температура '$temperature' C'
     sleep $timeout
