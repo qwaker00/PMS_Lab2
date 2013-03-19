31094a3592d24682539f85bbfb84caeccb2e2a2a
Eugene Gritskevich <qwaker.00@gmail.com>
2013-03-19 18:12:16 +0300

    Update weather script

--- Lab2/weather.sh
+++ Lab2/weather.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 timeout=`awk '{print $2}' weather.cfg`
-echo 'Update timeout:' $timeout 'second(s)'
-
-echo 'Will get info from http://export.yandex.ru/'
+echo 'Обновление :' $timeout 'секунд'
 
 while [[ 1 ]]
 do
-    weather=`wget -q -O - http://export.yandex.ru/weather-ng/forecasts/26850.xml | head -n20 | grep temperature | awk -F"[><]" '{print $3}'`
-    echo 'Weather in Minsk:' $weather
+    wget -q -O .weather.tmp http://export.yandex.ru/weather-ng/forecasts/26850.xml
+    weather=`head -n30 .weather.tmp | grep '\<weather_type\>' | awk -F"[><]" '{print $3}'`
+    temperature=`head -n30 .weather.tmp | grep temperature | awk -F"[><]" '{print $3}'`
+    rm .weather.tmp
+
+    echo 'Минск:' $weather', температура '$temperature' C'
     sleep $timeout
 done
 
